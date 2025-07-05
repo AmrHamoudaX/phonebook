a618b4a76d78005b337f77f624ab599a301ab0eb
Amr <legithamouda@gmail.com>
2025-07-05 17:15:27 +0300

    Feat: Add railway.toml for multi-service deployment and configure frontend URL

--- frontend/src/services/persons.js
+++ frontend/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = '/api/persons';
+const baseUrl = import.meta.env.VITE_BACKEND_URL + '/api/persons';
 
 const getAll = () => {
   return axios.get(baseUrl);
